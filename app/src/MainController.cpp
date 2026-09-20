89e241771bfad62232c9380db45b4a4694d5c238
Matija <matija.milovanovic55@gmail.com>
2026-09-20 15:16:12 +0200

    Close app on esc

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -9,16 +9,18 @@
 #include<MainController.hpp>
 #include<spdlog/spdlog.h>
 
+#include "engine/platform/PlatformController.hpp"
+
 namespace app {
     void MainController::initialize() {
         spdlog::info("Maincontrolor initialized");
     }
 
-    /* bool MainController::loop() {
-         auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
-         if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) {
-             return false;
-         }
-         return true;
-     }*/
+    bool MainController::loop() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        if (platform->key(engine::platform::KeyId::KEY_ESCAPE).is_down()) {
+            return false;
+        }
+        return true;
+    }
 } // app
